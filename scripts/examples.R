0b8dca82dbb3ecdb2f65be1d91c2fde9914d6740
kimhuenguyen <31039574+kimhuenguyen@users.noreply.github.com>
2020-11-05 12:04:47 +0100

    Update examples.R

--- scripts/examples.R
+++ scripts/examples.R
@@ -40,9 +40,9 @@ alpha <- 2*pnorm(n^.15,lower.tail=F)
 result.al <- result_algorithms(n, SAdj, nsim, maxcard, alpha, model="poisson",mu=5,mu.nois=0.5,theta=0.5,pi=0.7)
 round(apply(result.al,2,mean),2)[c(4,5,6,9,15,16,17,20,26,27,28,31,37,38,39,42,48,49,50,53,59,60,61,64)]
 
-result.al <- result_algorithms(n, SAdj, nsim, maxcard, alpha, model="poisson",mu=5,mu.nois=0.5,theta=0.5,pi=0.7)
+result.al <- result_algorithms(n, SAdj, nsim, maxcard, alpha, model="nb",mu=5,mu.nois=0.5,theta=0.5,pi=0.7)
 round(apply(result.al,2,mean),2)[c(4,5,6,9,15,16,17,20,26,27,28,31,37,38,39,42,48,49,50,53,59,60,61,64)]
 
-result.al <- result_algorithms(n, SAdj, nsim, maxcard, alpha, model="poisson",mu=5,mu.nois=0.5,theta=0.5,pi=0.7)
+result.al <- result_algorithms(n, SAdj, nsim, maxcard, alpha, model="zinb",mu=5,mu.nois=0.5,theta=0.5,pi=0.7)
 round(apply(result.al,2,mean),2)[c(4,5,6,9,15,16,17,20,26,27,28,31,37,38,39,42,48,49,50,53,59,60,61,64)]
 
